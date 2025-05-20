081189cf303547270d90a1289f43b6f9ebd58835
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-05-19 21:50:35 -0300

    cabeçalho para main

--- main.c
+++ main.c
@@ -1,33 +1,4 @@
-#include "pico/stdlib.h"
-#include "pico/bootrom.h"
-
-#include "hardware/gpio.h"
-#include "hardware/pwm.h"
-#include "hardware/adc.h"
-#include "hardware/i2c.h"
-#include "hardware/clocks.h"
-
-#include "lib/ssd1306.h"
-#include "lib/font.h"
-#include "lib/buzzer.h"
-#include "lib/ws2812.h"
-#include "ws2812.pio.h"
-
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-#include <stdio.h>
-
-#define I2C_PORT i2c1
-#define I2C_SDA 14
-#define I2C_SCL 15
-#define endereco 0x3C
-#define JOYSTICK_X 26
-#define JOYSTICK_Y 27
-#define LED_GREEN 11
-#define LED_RED  13
-#define BUZZER_PIN 21
-#define BUTTON_B 6
+#include "main.h"
 
 // Estrutura de posição do joystick
 typedef struct {
